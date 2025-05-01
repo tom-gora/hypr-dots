d13c84152db9fdedb88965b940e43089645edbb0
tom-gora <goratomasz@outlook.com>
2025-05-01 22:32:52 +0100

    learning to code good every day :D

--- .config/nvim/lua/tomeczku/core/lsp/diagnostics.lua
+++ .config/nvim/lua/tomeczku/core/lsp/diagnostics.lua
@@ -1,33 +1,24 @@
-local M, h = {}, require("tomeczku.core.keymaps.helpers")
+local M, s, h = {}, vim.diagnostic.severity, require("tomeczku.core.keymaps.helpers")
+
+local icons = {
+	[s.ERROR] = "",
+	[s.INFO] = "",
+	[s.WARN] = "",
+	[s.HINT] = "󰛩",
+}
 
 local setDiagnosticsFormatLine = function(diag)
 	if not string.match(diag.source:sub(-1), "[A-Za-z0-9]") then
 		diag.source = diag.source:sub(1, #diag.source - 1)
 	end
-	if diag.severity == vim.diagnostic.severity.ERROR then
-		return "  <--    " .. diag.source .. ":  " .. diag.message .. "  "
-	elseif diag.severity == vim.diagnostic.severity.WARN then
-		return "  <--    " .. diag.source .. ": " .. diag.message .. "  "
-	elseif diag.severity == vim.diagnostic.severity.INFO then
-		return "  <--    " .. diag.source .. ":  " .. diag.message .. "  "
-	elseif diag.severity == vim.diagnostic.severity.HINT then
-		return "  <--    " .. diag.source .. ":  " .. diag.message .. "  "
-	end
+	return "  <--  " .. icons[diag.severity] .. " " .. diag.source .. ":  " .. diag.message .. "  "
 end
 
 local setDiagnosticsFormatFloat = function(diag)
 	if not string.match(diag.source:sub(-1), "[A-Za-z0-9]") then
 		diag.source = diag.source:sub(1, #diag.source - 1)
 	end
-	if diag.severity == vim.diagnostic.severity.ERROR then
-		return "   " .. diag.source .. ":  " .. diag.message .. "  "
-	elseif diag.severity == vim.diagnostic.severity.WARN then
-		return "   " .. diag.source .. ": " .. diag.message .. "  "
-	elseif diag.severity == vim.diagnostic.severity.INFO then
-		return "   " .. diag.source .. ":  " .. diag.message .. "  "
-	elseif diag.severity == vim.diagnostic.severity.HINT then
-		return "   " .. diag.source .. ":  " .. diag.message .. "  "
-	end
+	return " " .. icons[diag.severity] .. " " .. diag.source .. ":  " .. diag.message .. "  "
 end
 
 M.setup = function()
@@ -36,8 +27,18 @@ M.setup = function()
 			format = setDiagnosticsFormatLine,
 			virt_text_pos = "eol_right_align",
 			hl_mode = "combine",
+			severity = s.WARN,
 		},
 		virtual_lines = false,
+		signs = {
+			priority = 100,
+			text = {
+				[s.ERROR] = icons[s.ERROR],
+				[s.WARN] = icons[s.WARN],
+				[s.INFO] = icons[s.INFO],
+				[s.HINT] = icons[s.HINT],
+			},
+		},
 		float = {
 			header = "",
 			format = setDiagnosticsFormatFloat,
